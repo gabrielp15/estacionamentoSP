7df5a0eb82eabda2ac858de1875a3dc61923c70d
Gabriel Pereira <104700476+gabrielp15@users.noreply.github.com>
2025-01-23 08:45:29 -0300

    Delete EstacionamentoSP directory
    Deletado arquivos importados em local incorreto.

--- EstacionamentoSP/src/main/java/com/spsoftware/estacionamentosp/config/config.java
+++
@@ -1,9 +0,0 @@
-package com.spsoftware.estacionamentosp.config;
-
-import com.vaadin.flow.spring.annotation.EnableVaadin;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableVaadin
-public class config {
-}
